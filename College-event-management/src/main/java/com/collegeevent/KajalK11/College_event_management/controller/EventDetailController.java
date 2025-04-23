416e0304e28bf7e4fb707ced58b681d1250d9cf9
Warisha-Irfan <warishairfan005@gmail.com>
2025-04-24 01:12:52 +0530

    EventDetail done by warisha!

---
+++ College-event-management/src/main/java/com/collegeevent/KajalK11/College_event_management/controller/EventDetailController.java
@@ -0,0 +1,37 @@
+package com.collegeevent.KajalK11.College_event_management.controller;
+
+
+
+import com.collegeevent.KajalK11.College_event_management.model.EventDetail;
+import com.collegeevent.KajalK11.College_event_management.service.EventDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/event-details")
+public class EventDetailController {
+
+    @Autowired
+    private EventDetailService service;
+
+    @PostMapping
+    public EventDetail create(@RequestBody EventDetail detail) {
+        return service.save(detail);
+    }
+
+    @GetMapping
+    public List<EventDetail> getAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public EventDetail getById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
---
+++ College-event-management/src/main/java/com/collegeevent/KajalK11/College_event_management/repository/EventDetailRepository.java
@@ -0,0 +1,10 @@
+package com.collegeevent.KajalK11.College_event_management.repository;
+
+
+import com.collegeevent.KajalK11.College_event_management.model.EventDetail;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventDetailRepository extends JpaRepository<EventDetail, Long> {
+}
+
